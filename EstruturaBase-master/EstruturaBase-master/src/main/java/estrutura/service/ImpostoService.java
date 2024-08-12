48bcdb99e536295a70a5a28ee6e6a0b398a4d022
arthurbernardxz <arthurber205@gmail.com>
2024-08-12 15:59:25 -0300

    Alterações finais

--- EstruturaBase-master/EstruturaBase-master/src/main/java/estrutura/service/ImpostoService.java
+++ EstruturaBase-master/EstruturaBase-master/src/main/java/estrutura/service/ImpostoService.java
@@ -7,16 +7,21 @@ public class ImpostoService {
     public double calcularImposto(double salario) {
         double imposto;
         if (salario < 22847.76) {
-            imposto = 0;
+            imposto = calcular(salario, 0, 0);
         } else if (salario < 33919.80) {
-            imposto = (salario * 0.075) - 1713.58;
+            imposto = calcular(salario, 0.075, 1713.58);
         } else if (salario < 45012.60) {
-            imposto = (salario * 0.15) - 4257.57;
+            imposto = calcular(salario, 0.15, 4257.57);
         } else if (salario < 55976.16) {
-            imposto = (salario * 0.225) - 7633.51;
+            imposto = calcular(salario, 0.225, 7633.51);
         } else {
-            imposto = (salario * 0.275) - 10432.32;
+            imposto = calcular(salario, 0.275, 10432.32);
         }
+        return imposto;
+    }
+    private double calcular(double salario, double taxa, double deducao){
+        double imposto;
+        imposto = (salario * taxa) - deducao;
         return imposto;
     }
 }
